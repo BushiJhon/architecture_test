d89a3cb2355b687a395563b795ee0a10c10f2b62
Kind-Jhon <1172905107@qq.com>
2019-12-12 16:26:57 +0800

    server file

--- app_server.py
+++ app_server.py
@@ -10,6 +10,7 @@ from module.result_produce import result_produce
 
 from bottle import *
 BaseRequest.MEMFILE_MAX = 10000 * 1000
+points_ping = []
 
 
 @route('/string_to_sketch', method='POST')
@@ -17,8 +18,6 @@ def remote_string_to_sketch():
     data = request.json
     image = data['image']
     points = data['points']
-    print(image)
-    print(points)
     sketch = string_to_sketch(image=image, points=points)
     sketch = sketch_blurry(sketch=sketch)
     std = judge_improve(sketch=sketch)
@@ -32,11 +31,13 @@ def remote_string_to_sketch():
     cv2.imwrite('result.jpg', result)
     return static_file('result.jpg', root='.')
 
-'''
+
 @route('/sketch_blurry', method='POST')
 def remote_sketch_blurry():
     sketch = request.files.get('sketch')
-    points = request.json['points']
+    sketch.save('sketch_blurry.jpg')
+    sketch = cv2.imread('sketch_blurry.jpg')
+    points = points_ping
     sketch = sketch_blurry(sketch=sketch)
     std = judge_improve(sketch=sketch)
     if std:
@@ -46,13 +47,16 @@ def remote_sketch_blurry():
     baby = baby_determine(sketch_128=sketch_128, points=points)
     composition = composition_determine(sketch_256=sketch_256, baby=baby)
     result = result_produce(sketch_1024=sketch_1024, points=points, composition=composition)
-    return result
+    cv2.imwrite('result.jpg', result)
+    return static_file('result.jpg', root='.')
 
 
 @route('/judge_improve', method='POST')
 def remote_judge_improve():
     sketch = request.files.get('sketch')
-    points = request.json['points']
+    sketch.save('judge_improve.jpg')
+    sketch = cv2.imread('judge_improve.jpg')
+    points = points_ping
     std = judge_improve(sketch=sketch)
     if std:
         sketch = continue_improve(sketch=sketch)
@@ -61,43 +65,53 @@ def remote_judge_improve():
     baby = baby_determine(sketch_128=sketch_128, points=points)
     composition = composition_determine(sketch_256=sketch_256, baby=baby)
     result = result_produce(sketch_1024=sketch_1024, points=points, composition=composition)
-    return result
+    cv2.imwrite('result.jpg', result)
+    return static_file('result.jpg', root='.')
 
 
 @route('/continue_improve', method='POST')
 def remote_continue_improve():
     sketch = request.files.get('sketch')
-    points = request.json['points']
+    sketch.save('continue_improve.jpg')
+    sketch = cv2.imread('continue_improve.jpg')
+    points = points_ping
     sketch = continue_improve(sketch=sketch)
     sketch = go_gray(sketch=sketch)
     sketch_1024, sketch_256, sketch_128 = sketch_norm(sketch=sketch)
     baby = baby_determine(sketch_128=sketch_128, points=points)
     composition = composition_determine(sketch_256=sketch_256, baby=baby)
     result = result_produce(sketch_1024=sketch_1024, points=points, composition=composition)
-    return result
+    cv2.imwrite('result.jpg', result)
+    return static_file('result.jpg', root='.')
 
 
 @route('/go_gray', method='POST')
 def remote_go_gray():
     sketch = request.files.get('sketch')
-    points = request.json['points']
+    sketch.save('go_gray.jpg')
+    sketch = cv2.imread('go_gray.jpg')
+    points = points_ping
     sketch = go_gray(sketch=sketch)
     sketch_1024, sketch_256, sketch_128 = sketch_norm(sketch=sketch)
     baby = baby_determine(sketch_128=sketch_128, points=points)
     composition = composition_determine(sketch_256=sketch_256, baby=baby)
     result = result_produce(sketch_1024=sketch_1024, points=points, composition=composition)
-    return result
+    cv2.imwrite('result.jpg', result)
+    return static_file('result.jpg', root='.')
 
 
 @route('/sketch_norm', method='POST')
 def remote_sketch_norm():
     sketch = request.files.get('sketch')
-    points = request.json['points']
+    sketch.save('sketch_norm.jpg')
+    sketch = cv2.imread('sketch_norm.jpg')
+    points = points_ping
     sketch_1024, sketch_256, sketch_128 = sketch_norm(sketch=sketch)
     baby = baby_determine(sketch_128=sketch_128, points=points)
     composition = composition_determine(sketch_256=sketch_256, baby=baby)
     result = result_produce(sketch_1024=sketch_1024, points=points, composition=composition)
-    return result
+    cv2.imwrite('result.jpg', result)
+    return static_file('result.jpg', root='.')
 
 
 @route('/baby_determine', method='POST')
@@ -105,11 +119,18 @@ def remote_baby_determine():
     sketch_128 = request.files.get('sketch_128')
     sketch_256 = request.files.get('sketch_256')
     sketch_1024 = request.files.get('sketch_1024')
-    points = request.json['points']
+    sketch_128.save('sketch_128.jpg')
+    sketch_256.save('sketch_256.jpg')
+    sketch_1024.save('sketch_1024.jpg')
+    sketch_128 = cv2.imread('sketch_128.jpg')
+    sketch_256 = cv2.imread('sketch_256.jpg')
+    sketch_1024 = cv2.imread('sketch_1024.jpg')
+    points = points_ping
     baby = baby_determine(sketch_128=sketch_128, points=points)
     composition = composition_determine(sketch_256=sketch_256, baby=baby)
     result = result_produce(sketch_1024=sketch_1024, points=points, composition=composition)
-    return result
+    cv2.imwrite('result.jpg', result)
+    return static_file('result.jpg', root='.')
 
 
 @route('/composition_determine', method='POST')
@@ -120,7 +141,8 @@ def remote_composition_determine():
     points = request.json['points']
     composition = composition_determine(sketch_256=sketch_256, baby=baby)
     result = result_produce(sketch_1024=sketch_1024, points=points, composition=composition)
-    return result
+    cv2.imwrite('result.jpg', result)
+    return static_file('result.jpg', root='.')
 
 
 @route('/result_produce', method='POST')
@@ -129,7 +151,8 @@ def remote_result_produce():
     sketch_1024 = request.files.get('sketch_1024')
     points = request.json['points']
     result = result_produce(sketch_1024=sketch_1024, points=points, composition=composition)
-    return result
-'''
+    cv2.imwrite('result.jpg', result)
+    return static_file('result.jpg', root='.')
+
 
 run(host='localhost', port=8080)
