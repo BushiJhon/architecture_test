e52d18f5cce0b7dd3298e0c387873c46b28cdb91
Kind-Jhon <1172905107@qq.com>
2019-12-12 16:26:38 +0800

    local file

--- app_local.py
+++ app_local.py
@@ -1,26 +1,79 @@
 import urllib3
 import json
-from image import *
-from PIL import Image
-from io import BytesIO
+# with open('result.jpg', 'rb') as f:
+    #     sketch = f.read()
 
 
-def http_post():
+def to_remote_string_to_sketch(image, points):
     url = 'http://localhost:8080/string_to_sketch'
     values = {'image': image, 'points': points}
-    jdata = json.dumps(values).encode('utf-8')
+    data = json.dumps(values).encode('utf-8')
 
     http = urllib3.PoolManager()
     res = http.request(
         'POST',
         url,
-        body=jdata,
+        body=data,
         headers={'Content-Type': 'application/json'}
     )
     return res.data
 
 
-image = http_post()
-bytes_stream = BytesIO(image)
-image = Image.open(bytes_stream)
-image.show()
+def to_remote_sketch_blurry(sketch, points):
+    url = 'http://localhost:8080/sketch_blurry'
+    http = urllib3.PoolManager()
+    res = http.request(
+        'POST',
+        url,
+        fields={
+            'sketch': ('sketch.jpg', sketch, 'image/jpg'),
+            'points': str(points),
+        }
+    )
+    return res.data
+
+# from image import *
+# to_remote_string_to_sketch(image, points)
+
+
+# with open('sketch.jpg', 'rb') as f:
+#         sketch = f.read()
+#
+#
+# from image import points
+#
+# points = points
+# print(points)
+# for _ in range(len(points)):
+#     points[_][1] = 1 - points[_][1]
+
+# to_remote_sketch_blurry(sketch, points)
+'''
+def to_remote_judge_improve():
+    return res.data
+
+
+def to_remote_continue_improve():
+    return res.data
+
+
+def to_remote_go_gray():
+    return res.data
+
+
+def to_remote_sketch_norm():
+    return res.data
+
+
+def to_remote_baby_determine():
+    return res.data
+
+
+def to_remote_composition_determine():
+    return res.data
+
+
+def to_remote_result_produce():
+    return res.data
+    
+'''
